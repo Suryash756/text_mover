da7d97510bceaaea60bf063afddb718fda2ea872
Suryash756 <86527278+Suryash756@users.noreply.github.com>
2023-08-26 22:43:58 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 function setup()
 {
     video = createCapture(VIDEO);
@@ -9,10 +10,20 @@ function setup()
 
     poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on('pose',gotPoses);
+
+    
 }
 
 function draw(){
     background('#969A97');
+    value1 = document.getElementById("value_user").value;
+
+    document.getElementById("square_side").innerHTML = "size of text = "+difference +"px";
+    fill('#F90093');
+    
+    textSize(difference);
+    text(value1,noseX, noseY);
+    
 }
 
 function modelLoaded()
@@ -20,11 +31,26 @@ function modelLoaded()
     console.log('poseNet is Initilized');
 }
 
+noseX=0;
+noseY=0;
+difference = 0;
+rightWristX = 0;
+leftWristX =0;
+
 function gotPoses(results)
 {
 
     if(results.length>0)
     {
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose X = "+noseX+"nose Y ="+noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX-rightWristX);
+
+        console.log("leftWristX = "+leftWristX+"rightWristX ="+rightWristX+"difference = "+difference);
     }
 }
